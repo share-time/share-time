5b827970050337849734d74dbd87272228546ae9
gypang <gypang24@gmail.com>
2018-04-14 20:15:10 -0700

    added first round of viewcontrollers dun dun dun

---
+++ share-time/ClassDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ClassDetailViewController.swift
+//  share-time
+//
+//  Created by Godwin Pang on 4/14/18.
+//  Copyright © 2018 share-time. All rights reserved.
+//
+
+import UIKit
+
+class ClassDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
