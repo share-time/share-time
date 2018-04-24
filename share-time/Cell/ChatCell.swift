d110b291543d54b22caff66de45505d503d57ce1
Guanxin <liguanxin078@gmail.com>
2018-04-24 05:45:23 -0700

    parse chat

--- share-time/Cell/ChatCell.swift
+++ share-time/Cell/ChatCell.swift
@@ -11,9 +11,13 @@ import UIKit
 class ChatCell: UITableViewCell {
 
     @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var bubbleView: UIView!
     @IBOutlet weak var messageLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
+        bubbleView.layer.cornerRadius = 16
+        bubbleView.clipsToBounds = true
+        bubbleView.backgroundColor = UIColor(red:0.02, green:0.47, blue:0.98, alpha:1.0)
         // Initialization code
     }
 
--- share-time/View Controllers/ChatViewController.swift
+++ share-time/View Controllers/ChatViewController.swift
@@ -7,34 +7,77 @@
 //
 
 import UIKit
+import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBAction func onSend(_ sender: UIButton) {
-    }
+    
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    var chatMessage: [PFObject]? = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 50
+        tableView.separatorStyle = .none
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ChatViewController.onTimer), userInfo: nil, repeats: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int {
+        if let chatMessage = chatMessage {
+            return chatMessage.count
+        } else {
+            return 0
+        }
     }
-    */
+    
+    @objc func onTimer() {
+        let query = PFQuery(className: "Message")
+        query.includeKey("user")
+        query.addDescendingOrder("createdAt")
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) in
+            if let posts = posts {
+                self.chatMessage = posts
+                self.tableView.reloadData()
+            } else {
+                print("Problem refreshing message: \(error!.localizedDescription)")
+            }
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatCell
+        let messages = chatMessage?[indexPath.row]
+        if let msgString = messages?["text"] as? String {
+            cell.messageLabel.text = msgString
+        }
+        
+        if let user = messages?["user"] as? PFUser {
+            print("The user is: \(user)")
+            cell.usernameLabel.text = user.username
+        } else {
+            // No user found, set default username
+            cell.usernameLabel.text = "🤖"
+        }
+        return cell
+    }
 
+    @IBAction func onSend(_ sender: UIButton) {
+        let sendObject = PFObject(className: "Message")
+        sendObject["text"] = messageTextField.text ?? ""
+        sendObject["user"] = PFUser.current()
+        sendObject.saveInBackground { (success, error) in
+            if success {
+                print("The message was saved!")
+                self.messageTextField.text = ""
+            } else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        }
+    }
 }
