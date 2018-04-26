b13e8d94a8be72c7f4301ddf99e4f8427ce9e6f0
gypang <gypang24@gmail.com>
2018-04-25 18:41:08 -0700

    added custom pfobject classes

---
+++ share-time/Course.swift
@@ -0,0 +1,9 @@
+//
+//  Course.swift
+//  share-time
+//
+//  Created by Godwin Pang on 4/25/18.
+//  Copyright © 2018 share-time. All rights reserved.
+//
+
+import Foundation
---
+++ share-time/StudyGroup.swift
@@ -0,0 +1,9 @@
+//
+//  StudyGroup.swift
+//  share-time
+//
+//  Created by Godwin Pang on 4/25/18.
+//  Copyright © 2018 share-time. All rights reserved.
+//
+
+import Foundation
