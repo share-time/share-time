b632630bf08dc2d15dc8283efbc7204b0a57706e
Guanxin <liguanxin078@gmail.com>
2018-05-09 18:51:24 -0700

    change password done

--- share-time/View Controllers/ChangePasswordViewController.swift
+++ share-time/View Controllers/ChangePasswordViewController.swift
@@ -13,8 +13,7 @@ class ChangePasswordViewController: UIViewController {
 
     let signUpConfirmErrorAlertController = UIAlertController(title: "Passwords do not match", message: "Please enter again", preferredStyle: .alert)
     let signUpPasswordErrorAlertController = UIAlertController(title: "Incorrect Password", message: "Please re-enter password", preferredStyle: .alert)
-    let signUpPasswordSuccessAlertController = UIAlertController(title: "Success", message: "Changed password successfully!!", preferredStyle: .alert)
-    let samePasswordAlertController = UIAlertController(title: "Invalid Password", message: "New password cannot be same as the old password", preferredStyle: .alert)
+    let signUpPasswordSuccessAlertController = UIAlertController(title: "Success", message: "Changed password successfully!", preferredStyle: .alert)
     let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
         //does nothing -> dismisses alert view
     }
@@ -29,6 +28,7 @@ class ChangePasswordViewController: UIViewController {
         super.viewDidLoad()
         signUpConfirmErrorAlertController.addAction(OKAction)
         signUpPasswordErrorAlertController.addAction(OKAction)
+        signUpPasswordSuccessAlertController.addAction(OKAction)
         // Do any additional setup after loading the view.
     }
 
@@ -37,29 +37,35 @@ class ChangePasswordViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func clearPassword() {
+        currentPasswordTextField.text = ""
+        newPasswordTextField.text = ""
+        confirmPasswordTextField.text = ""
+    }
 
     @IBAction func onPasswordSave(_ sender: Any) {
 
         PFUser.logInWithUsername(inBackground: (self.user?.username!)!, password: self.currentPasswordTextField.text!) { (user: PFUser?, error: Error?) in
             if error != nil {
                 self.present(self.signUpPasswordErrorAlertController, animated: true)
-            }
-        }
-        if (newPasswordTextField.text == user?.password){
-            self.present(self.samePasswordAlertController, animated: true)
-        }
-        if (newPasswordTextField.text != confirmPasswordTextField.text){
-            present(signUpConfirmErrorAlertController, animated: true)
-        } else {
-            user?.password = newPasswordTextField.text
-            user?.saveInBackground{ (success: Bool, error: Error?) -> Void in
-                if (success){
-                    self.present(self.signUpPasswordSuccessAlertController, animated: true)
-                    if let navController = self.navigationController{
-                        navController.popViewController(animated: true)
-                    }
+                self.clearPassword()
+            } else {
+                if (self.newPasswordTextField.text != self.confirmPasswordTextField.text){
+                    self.present(self.signUpConfirmErrorAlertController, animated: true)
+                    self.clearPassword()
                 } else {
-                    print("could not save password")
+                    user?.password = self.newPasswordTextField.text
+                    user?.saveInBackground{ (success: Bool, error: Error?) -> Void in
+                        if (success){
+                            self.present(self.signUpPasswordSuccessAlertController, animated: true)
+                            if let navController = self.navigationController{
+                                navController.popViewController(animated: true)
+                            }
+                            self.clearPassword()
+                        } else {
+                            print("could not save password")
+                        }
+                    }
                 }
             }
         }
