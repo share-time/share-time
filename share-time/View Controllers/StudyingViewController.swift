4f6fbd9bf19bf0db8805bafee1bce572bb9e9c10
gypang <gypang24@gmail.com>
2018-05-12 18:32:10 -0700

    added empty vc as placeholder for studying view

---
+++ share-time/View Controllers/StudyingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  StudyingViewController.swift
+//  share-time
+//
+//  Created by Godwin Pang on 5/12/18.
+//  Copyright © 2018 share-time. All rights reserved.
+//
+
+import UIKit
+
+class StudyingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
