1d6ed77b73028d819d27f0316ef8bf778b222bd2
gypang <gypang24@gmail.com>
2018-05-12 18:37:13 -0700

    added code for proximity sensor based timer

--- share-time/View Controllers/StudyingViewController.swift
+++ share-time/View Controllers/StudyingViewController.swift
@@ -9,13 +9,38 @@
 import UIKit
 
 class StudyingViewController: UIViewController {
+    
+    let device = UIDevice.current
+    var counter = 0.0
+    var timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        device.isProximityMonitoringEnabled = true
+        if (device.isProximityMonitoringEnabled){
+            NotificationCenter.default.addObserver(self, selector: #selector(ViewController.proximityChanged), name: Notification.Name(rawValue: "UIDeviceProximityStateDidChangeNotification"), object: device)
+        }
     }
 
+    @objc func proximityChanged(notification: Notification){
+        if let device = notification.object as? UIDevice{
+            print("\(device) detected!")
+            print("proximityState: " + String(device.proximityState))
+            if (device.proximityState){
+                timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+            } else {
+                print("Time interval: \(counter)")
+                timer.invalidate()
+                counter = 0
+            }
+        }
+    }
+    
+    @objc func updateTimer(){
+        counter = counter + 1
+        print(counter)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
