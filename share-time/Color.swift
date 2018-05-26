826ae3fadca1804ff731f05a065169b956ed62e6
Guanxin <liguanxin078@gmail.com>
2018-05-26 07:05:29 -0700

    colors

--- share-time/Color.swift
+++ share-time/Color.swift
@@ -13,6 +13,14 @@ class Color {
     static let lightGreyColor = UIColor(red: 197/255, green: 205/255, blue: 205/255, alpha: 1.0)
     static let darkGreyColor = UIColor(red: 52/255, green: 42/255, blue: 61/255, alpha: 1.0)
     static let overcastBlueColor = UIColor(red: 0, green: 187/255, blue: 204/255, alpha: 1.0)
+    // For the gradient background
     static let darkRedPinkColor = UIColor(red: 204/255, green: 43/255, blue: 94/255, alpha: 1)
     static let darkPurpleColor = UIColor(red: 117/255, green: 58/255, blue: 136/255, alpha: 1)
+    
+    // Color scheme
+    static let superDarkPurple = UIColor(red: 56/255, green: 2/255, blue: 59/255, alpha: 1)
+    static let lightPurple = UIColor(red: 162/255, green: 136/255, blue: 227/255, alpha: 1)
+    static let mediumBlue = UIColor(red: 187/255, green: 213/255, blue: 237/255, alpha: 1)
+    static let lightBlue = UIColor(red: 206/255, green: 253/255, blue: 255/255, alpha: 1)
+    static let lightGreen = UIColor(red: 204/255, green: 255/255, blue: 203/255, alpha: 1)
 }
